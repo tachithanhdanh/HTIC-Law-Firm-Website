b943a379b74534e56847ad4701a3b60e37e0db94
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-12-23 17:04:44 +0700

    Add navbar

--- src/pages/consult.tsx
+++ src/pages/consult.tsx
@@ -1,9 +1,6 @@
-import Navbar from "@/components/Navbar";
-
 const Consult = () => {
   return (
     <div>
-      <Navbar />
       <h1>Tư Vấn Pháp Lý</h1>
       <p>Liên hệ chúng tôi để nhận tư vấn pháp lý chuyên sâu.</p>
     </div>
