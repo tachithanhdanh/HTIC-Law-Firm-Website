3845b85b618d68abb804227b7e30a4cc663bbe1a
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-12-25 23:57:12 +0700

    Remove redudant line from consult page

--- src/pages/consult.tsx
+++ src/pages/consult.tsx
@@ -6,10 +6,6 @@ const Consult = () => {
     <>
       <ConsultHeader />
       <ConsultBody />
-      <div>
-        <h1>Tư Vấn Pháp Lý</h1>
-        <p>Liên hệ chúng tôi để nhận tư vấn pháp lý chuyên sâu.</p>
-      </div>
     </>
   );
 };
